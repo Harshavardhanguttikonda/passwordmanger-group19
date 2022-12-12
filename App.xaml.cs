bf3719b1049ee7f0e0a436b40de3e3ed0316a013
Harshavardhanguttikonda <115022190+Harshavardhanguttikonda@users.noreply.github.com>
2022-12-11 21:16:27 -0600

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,8 +5,8 @@ public partial class App : Application
 	public static PmViewModel vm;
 	public App()
 	{
-		InitializeComponent();
-		vm = new PmViewModel();
-		MainPage = new NavigationPage(new LoginPage());
+		
+		
+		
 	}
 }
