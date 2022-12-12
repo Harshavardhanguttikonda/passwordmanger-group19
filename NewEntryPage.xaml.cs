11f7ddbc769e0b91e09208760a16a50ddc2f310a
Harshavardhanguttikonda <115022190+Harshavardhanguttikonda@users.noreply.github.com>
2022-12-11 21:56:16 -0600

    Update NewEntryPage.xaml.cs

--- NewEntryPage.xaml.cs
+++ NewEntryPage.xaml.cs
@@ -11,7 +11,7 @@ public partial class NewEntryPage : ContentPage
 
 	private void Entry_TextChanged(object sender, TextChangedEventArgs e)
 	{
-		appname = e.NewTextValue;
+		
 	}
 
 	private void Entry_TextChanged_1(object sender, TextChangedEventArgs e)
